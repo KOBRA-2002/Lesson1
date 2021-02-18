0e13b41ade51a98b7b425693682d57b7dfa65b5e
KOBRA-2002 <kobzarev.dani@yandex.ru>
2021-02-18 22:07:58 +0300

    cos(x)

--- Lesson1/Lesson1.cpp
+++ Lesson1/Lesson1.cpp
@@ -1,8 +1,31 @@
 ﻿#include <iostream>
 // Для запуска проекта нажать ctrl + f5
+// endl - Манипулятор. Манипулятор это объект для управления потоковым вводом выводом
 using namespace std;
+
+const double E = 10e-5;
 
 int main()
 {
-    cout << "Hello World!\n";
+	double x = 0;
+	double s = 0;
+	cout << "What is your argument for cos(x)?";
+	cin >> x;
+	int c = 1;
+	int n = 0;
+	double a = 1;  
+	double p = 1;
+	double q = 1;
+	while (abs(a) > E)
+	{
+		s += a;  
+		c *= -1;  // Знак слагаемого 
+		p *= x * x;  // x^(2n)
+		n += 2;
+		q *= n;  // 2n! - Двойной факториал 
+		a = c * p / q;
+		cout << a << endl;
+	}
+	cout << "Your result is ";
+	cout << s;
 }
